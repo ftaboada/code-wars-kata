4b32b97c32241d4d31a348dd050cef89032200d4
n4gual <ftaboada@gmail.com>
2020-08-06 11:57:38 -0400

    more 8kyu katas

--- katas.js
+++ katas.js
@@ -62,6 +62,91 @@ function repeatStr(n, s) {
   }
   return string;
 }
+//is n divisible by x and y?
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0) {
+    return true;
+  }
+  return false;
+}
+//keep hydrated!
+function litres(time) {
+  let res = Math.floor(time / 2);
+
+  return res;
+}
+//Fake Binary
+function fakeBin(x) {
+  let xlen = x.length;
+  let resultado = "";
+  for (let i = 0; i < xlen; i++) {
+    if (x[i] < 5) {
+      resultado += "0";
+    } else {
+      resultado += "1";
+    }
+  }
+  return resultado;
+}
+// Short long Short
+function solution(a, b) {
+  if (a === "" || b === "") {
+    console.log(a + b);
+    return a + b;
+  }
+  if (a.length < b.length) {
+    console.log("solution -> a + b + a", a + b + a);
+    return a + b + a;
+  } else {
+    console.log("solution -> b + a + b", b + a + b);
+    return b + a + b;
+  }
+}
+// Find Multiples of a Number
+function findMultiples(integer, limit) {
+  let result = [];
+  for (let i = integer; i <= limit; i++) {
+    if (i % interger === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+//Check the exam
+function checkExam(array1, array2) {
+  let len = array1.length;
+  let resultado = 0;
+  for (let i = 0; i < len; i++) {
+    if (array1[i] === array2[i]) {
+      resultado += 4;
+    } else if (array2[i] !== "" && array2[i] !== array1[i]) {
+      resultado -= 1;
+    }
+  }
+  if (resultado < 0) {
+    return 0;
+  }
+  return resultado;
+}
+//l1: Bartender, drinks!
+function getDrinkByProfession(param) {
+  let np = param.toUpperCase();
+  if (np === "JABRONI") {
+    return "Patron Tequila";
+  } else if (np === "SCHOOL COUNSELOR") {
+    return "Anything with Alcohol";
+  } else if (np === "PROGRAMMER") {
+    return "Hipster Craft Beer";
+  } else if (np === "BIKE GANG MEMBER") {
+    return "Moonshine";
+  } else if (np === "POLITICIAN") {
+    return "Your tax dollars";
+  } else if (np === "RAPPER") {
+    return "Cristal";
+  } else {
+    return "Beer";
+  }
+}
 //simple string reversal 7kyu
 function solve(str) {
   let strlen = str.length - 1;
@@ -71,3 +156,4 @@ function solve(str) {
   }
   return newstr.toString();
 }
+// Longest alphabetical substring
